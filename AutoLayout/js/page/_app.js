94b5fe1c377b1f53dd4a3d4e54b43d66723db180
sunkaixuan <610262374@qq.com>
2015-06-12 14:24:26 +0800

    add project

---
+++ AutoLayout/js/page/_app.js
@@ -0,0 +1,9 @@
+﻿var layoutApp = angular.module('layoutApp', []);
+var jqApp = jQuery;
+jqApp.controller = function (jqApp, init) {
+    if (init != null) {
+        $(function () {
+            init();
+        })
+    }
+}
---
+++ AutoLayout/js/page/_layout.js
@@ -0,0 +1,16 @@
+﻿layoutApp.controller('headController',
+ function ($scope) {
+     $scope.title = "美女画画计区";
+ });
+
+layoutApp.controller('bodyController',
+function ($scope) {
+    $scope.forum = [
+        { title: "画画交流", img: "forumicon_20.jpg" },
+        { title: "临摹素材", img: "forumicon_20.jpg" },
+        { title: "临摹素材", img: "forumicon_20.jpg" },
+        { title: "临摹素材", img: "forumicon_20.jpg" },
+        { title: "临摹素材", img: "forumicon_20.jpg" },
+        { title: "临摹素材", img: "forumicon_20.jpg" }];
+})
+ 
